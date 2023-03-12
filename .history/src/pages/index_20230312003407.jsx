4ca33286f02f3c2707eefc5b8fc833f6b7b72698
northrn <44729398+northrn@users.noreply.github.com>
2023-03-12 11:49:21 -0400

    first commit

---
+++ .history/src/pages/index_20230312003407.jsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Pricing } from '@/components/Pricing'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+import { Banner } from '@/components/Banner'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Audience King - Custom Sales Systems for Creator Businesses</title>
+        <meta
+        property="og:image"
+        content="https://og-examples.vercel.sh/api/static"
+        name="description"
+        />
+      </Head>
+      <Banner />
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Pricing />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
